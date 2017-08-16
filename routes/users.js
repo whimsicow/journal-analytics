d0e9640239b667765080b4aa44025608ccb4224d
Katie <katherinelane11@gmail.com>
2017-08-16 14:32:05 -0400

    adjusts users and layout

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,11 @@ var router = express.Router();
 // const db = require('../db');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//   res.send('respond with a resource');
+  res.render('users', {
+    title: "The Swan House",
+    message: "Choose a member to see their status.",
+  })
 });
 
 module.exports = router;
