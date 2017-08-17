72536b5834de74a684528e35210a5faf12c3ee24
Mac User <sabbey37@gmail.com>
2017-08-17 08:25:47 -0400

    Shows user's name upon login

--- routes/users.js
+++ routes/users.js
@@ -12,17 +12,16 @@ function ensureAuthenticated(req, res, next) {
 router.get('/', ensureAuthenticated, function(req, res) {
     let user = '';
     user = JSON.stringify(req.user, null, 4);
-    console.log(user);
 
     res.render('users', {
         title: "The Swan House",
+        name: req.user.name.givenName,
         ftrlink: '/logout',
         ftrlinktext: 'Logout',
         navlink: '/logout',
         navlinktext: 'Logout'
     });
 });
-
 
 
 module.exports = router;
