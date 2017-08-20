c1a23e27e597ec6e272bb7b735db346c36c93e54
Katie <katherinelane11@gmail.com>
2017-08-20 14:19:24 -0400

    adds comments to form.js

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -4,6 +4,7 @@ const $CLOSE_POPUP = $('[data-popup="close-event-popup"]');
 
 var eventData = {};
 
+//gathers all information user intered into form and saves it to local storage
 const saveForm = () => {
     $FORM_CONTAINER.submit(() => {
         event.preventDefault();
@@ -20,6 +21,7 @@ const saveForm = () => {
     })
 }
 
+// resets form if reset button is clicked
 const resetButton = () => {
     $('[data-role="reset"]').click(() => {
         event.preventDefault();
@@ -29,31 +31,34 @@ const resetButton = () => {
         resetForm();
     })
 }
-
+// when form is reset, the date changes back to current date 
 const resetForm = () => {
     document.forms["eventform"].reset()
     setDefaultDate();
 }
-
+// stores event in database
 const dbStoreEvent = () => {
     $.post('/api/eventstore', eventData)
         .then((res) => {
             $('[data-role="status-msg"]').append(res);
         })
+        // catches error if any issues with savinmg 
         .catch((err) => {
             var $status = $('<span></span>', {
                 'text': 'Sorry, your event could not be added at this time. Please try again.',
                 'class': 'status-msg'
             });
+            // prints to DOM that event was successfully submitted
             $('[data-role="status-msg"]').append($status);
         })
 }
-
+// stores optional link to local storage
 const getLink = () => {
     var link = $('[name="link"]').val();
     setLocalStorageValues('eventlink', link);
 }
 
+// get the account property info to store in database
 const getAccount = () => {
     var infoArray = $('#view-selector-container2 > .ViewSelector2 > .ViewSelector2-item > .FormField').find(":selected");
     var accountName = infoArray[0]['text'];
@@ -66,33 +71,36 @@ const getAccount = () => {
     setLocalStorageValues('propertyId', propertyId);
 }
 
+// stores description in form submition to local storage
 const getFormDescription = () => {
     var description = 'description';
     var $descrptionValue = $('[data-type="form-description"]').val();
     setLocalStorageValues(description, $descrptionValue);
 }
 
+// gets date value of how local storage saves date data and saves to local storage
 const getDate = () => {
     var date = 'date';
     var dateValue = new Date($('input[name="date"]').val());
     dateValue = new Date( dateValue.getTime() - dateValue.getTimezoneOffset() * -60000 ).toUTCString();
     // could take off timestamps:
     // dateValue=dateValue.split(' ').slice(0, 4).join(' ')
-  
     setLocalStorageValues(date, dateValue);
 }
 
+// sets default date of form to current day
 const setDefaultDate = () => {
     var d = new Date();
     document.getElementById('date').valueAsDate = new Date(d.getFullYear(), d.getMonth(), d.getDate(), 12);
 }
-
+// method is dropdown list of icons. can selet one and save to database to be used later for overlay of maps and gathering further information
 function getMethod() {
     var method = $('#myDropdown').find(":selected");
     method = method['prevObject'][0]['innerText'];
     setLocalStorageValues('method', method);
 }
 
+// how local storage is set
 const setLocalStorageValues = (key, keyValue) => {
     localStorage.setItem(key, keyValue);
     eventData[key] = keyValue;
@@ -109,16 +117,19 @@ const plusSignButton = () => {
         openEventAddForm();
     });
 };
+
+// closes popup form
 const closePopupButton = () => {
     $CLOSE_POPUP.click((event) => {
         event.preventDefault();
         closeEventAddForm();
     });
 };
-
+// opens popup form
 const openEventAddForm = () => {
     $FORM_CONTAINER.show('slow');
 };
+// closes popup form
 const closeEventAddForm = () => {
     $FORM_CONTAINER.hide('slow');
 };
