b06693ba1dbe4702d5bbf901f76b6d76ea2cd880
Mac User <sabbey37@gmail.com>
2017-08-20 13:56:07 -0400

    Get acct name/dates from form

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,7 @@ const router = express.Router();
 const db = require('../db')
 
 router.get('/events', (req, res, next) => {
-  //pseudo code for db query needs teamid from tables... foreign key to events table then select * .then()
-  // const fakeDBData = {
-  //   event_id: 1,
-  //   event_date: '08-18-2017',
-  //   team_id: 1324234,
-  //   description: 'demo day announcement',
-  //   method: 'tweet',
-  //   user_id: 123143432235
-  // }
+    console.log(req.body);
   db.query(`
     SELECT *
       from events
