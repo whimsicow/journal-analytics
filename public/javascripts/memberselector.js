5255fb874ca1dfd6e082e3f4ed874a7f7e57dd82
Mac User <sabbey37@gmail.com>
2017-08-21 13:15:01 -0400

    Add back google auth container

--- public/javascripts/memberselector.js
+++ public/javascripts/memberselector.js
@@ -2,6 +2,17 @@ const $MEMBERLIST = $('[data-role="member-list"]');
 
 gapi.analytics.ready(() => {
     
+    gapi.analytics.auth.authorize({
+        // auth-container is dom element that hosts the sign-in button during a sessions first load. sign in button can also contain an event listener to do something     else as well
+        container: 'embed-api-auth-container2',
+        //client ID of our project from developers console (using Sarahs)
+        clientid: CLIENT_ID,
+    })
+
+    // return user info to console when they sign in... (name, email, profilePic)
+    gapi.analytics.auth.on('signIn', function() {
+     
+    })
     const viewSelectorMembers = new gapi.analytics.ext.ViewSelector2({
         container: 'members-view-selector-container'
         })
