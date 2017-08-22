90a11a3f783a352fd3c7c10c78c2247c5bbd9f8e
Katie <katherinelane11@gmail.com>
2017-08-22 15:36:38 -0400

    adjusting responsiveness

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -14,3 +14,13 @@
 //             graph.catpureEventsData(results[0]);
 //         })
 // })
+
+
+// Wait for window load
+$(window).load(function() {
+    // Animate loader off screen
+    $("#loader").animate({
+        top: -200
+    }, 1500);
+});
+	
